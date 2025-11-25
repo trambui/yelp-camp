2936f9eba7cd644cf3fae8869c3602c73b75972d
Tram Bui <trambui26@gmail.com>
2025-11-25 11:50:56 +0700

    fix warning

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const mongoSanitize = require('express-mongo-sanitize');
 
+mongoose.set('strictQuery', false);
+
 // const dbUrl = process.env.DB_URL;
 // const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
